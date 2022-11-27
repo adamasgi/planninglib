70e6d5fac149767aedc5d20308ca56161a4e5f48
AdamAsgi <git@adamasgi.com>
2022-11-26 17:33:48 -0800

    Exported api with proper string port

--- api.go
+++ api.go
@@ -16,10 +16,10 @@ func addItemSchedule(w http.ResponseWriter, r *http.Request, p httprouter.Params
 func getItem(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 }
 
-func Api(port int) {
+func Api(port string) {
 	router := httprouter.New()
 	router.POST("/item", newItem)
 	router.GET("/:item", getItem)
 
-	log.Fatal(http.ListenAndServe(port, router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
